af84d003754f8af6c144f35969e45fea0d74b143
balazstasi <balazstasi2@gmail.com>
2020-09-28 09:39:20 +0200

    Exercise solutions added

---
+++ react/Feladatok/Alap szintaxis (megoldott)/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+  state = {
+    username: 'balazstasi'
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="balazs" />
+      </div>
+    );
+  }
+}
+
+export default App;
