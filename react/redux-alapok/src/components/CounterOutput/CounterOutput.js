a6eb62e0b0cdb5b0185e1bb4bc02426ed91e5a89
balazstasi <balazstasi2@gmail.com>
2020-10-15 20:25:19 +0200

    asd

---
+++ react/redux-alapok/src/components/CounterOutput/CounterOutput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const counterOutput = (props) => (
+  <div className="CounterOutput">Current Counter: {props.value}</div>
+);
+
+export default counterOutput;
---
+++ react/redux-alapok/src/containers/Counter.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import CounterControl from "../components/CounterControl/CounterControl";
+import CounterOutput from "../components/CounterControl/CounterOutput";
+
+export class Counter extends Component {
+  state = {
+    counter: 0,
+  };
+
+  counterChangeHandler = (action, value) => {
+    // eslint-disable-next-line default-case
+    switch (action) {
+      case "inc":
+        this.setState((oldState) => {
+          return { counter: oldState + 1 };
+        });
+        break;
+      case "dec":
+        this.setState((oldState) => {
+          return { counter: oldState - 1 };
+        });
+        break;
+      case "add":
+        this.setState((oldState) => {
+          return { counter: oldState + value };
+        });
+        break;
+      case "sub":
+        this.setState((oldState) => {
+          return { counter: oldState - value };
+        });
+        break;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.counter} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.counterChangeHandler("dec")}
+        />
+        <CounterControl
+          label="Add 5"
+          clicked={this.counterChangeHandler("add", 5)}
+        />
+        <CounterControl
+          label="Subtract 15"
+          clicked={this.counterChangeHandler("sub", 15)}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
